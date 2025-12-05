bbf4c5eac7c62df6502eeee6ac6bd8754b1da723
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-12-05 13:02:05 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:ecommerce_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,30 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Shopping App",
-      home: HomePage(),
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(255, 225, 30, 1.0), ),
-        inputDecorationTheme: InputDecorationTheme(
-          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
-          hintStyle: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        textTheme: TextTheme(
-          titleMedium: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-          bodySmall: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-
-          )
-        ),
-      ),
-      
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp(home: Scaffold(body: Center(child: Text('Lets Start'))));
   }
 }
