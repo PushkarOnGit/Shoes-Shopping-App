7f424bd53fb7f3f6afbd2133c3511135bc937e59
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-12-06 22:34:19 +0530

    everything before using provider

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ecommerce_app/global_variable.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,6 +11,29 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Cart Page")));
+    return Scaffold(
+      appBar: AppBar(title: Text('Cart')),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage(cartItem['imageUrl'].toString()),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.red, size: 30),
+            ),
+            title: Text(
+              cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text("Size: ${cartItem['size'].toString()}"),
+          );
+        },
+      ),
+    );
   }
 }
